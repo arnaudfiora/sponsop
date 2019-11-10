f9e9cda3ae887315b3ffccaf0044999e4f526961
Arnaud Fiora <arnaud.fiora@gmail.com>
2019-11-10 21:55:26 +0100

    added banner + js to slide panels on click event

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,16 +1,42 @@
-console.log('home javascript initialised')
+// Make the page scroll to the log-in/sign-up section when clicking on the button
+const startButton = document.getElementById('get-started');
+const homeBanner = document.getElementById('home-banner');
+const homeBannerHeight = homeBanner.scrollHeight;
 
-const brandButton = document.getElementById('btnBrand');
-const creatorButton = document.getElementById('btnCreator');
+const scrollToFormSection = () => {
+  window.scroll({
+    top: homeBannerHeight,
+    behavior: 'smooth'
+  });
+};
+
+startButton.addEventListener('click', scrollToFormSection);
+
+// Make the forms appear
+const brandButton = document.getElementById('button-brand');
+const creatorButton = document.getElementById('button-creator');
+
 const overlayLeft = document.querySelector('.overlay-left')
-const overlayRight = document.querySelector('.overlay-right')
+const overlayRight = document.querySelector('.overlay-right');
 
+// Make the opposite panel move when clicking on a button
 brandButton.addEventListener('click', () => {
-    overlayLeft.classList.remove('left-panel-active');
-    overlayRight.classList.add('right-panel-active');
+    overlayRight.classList.toggle('right-panel-active');
+    scrollToFormSection();
 });
 
 creatorButton.addEventListener('click', () => {
+    overlayLeft.classList.toggle('left-panel-active');
+    scrollToFormSection();
+});
+
+// Putting it back in place when clicking on the remaining part of the div
+overlayRight.addEventListener('click', () => {
     overlayRight.classList.remove('right-panel-active');
-    overlayLeft.classList.add('left-panel-active');
+    scrollToFormSection();
+});
+
+overlayLeft.addEventListener('click', () => {
+    overlayLeft.classList.remove('left-panel-active');
+    scrollToFormSection();
 });
