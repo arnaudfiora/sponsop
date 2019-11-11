faa50469c760ceea6d1c05fc32f3d998ad38920c
Arnaud Fiora <arnaud.fiora@gmail.com>
2019-11-11 17:25:42 +0100

    sign-up page partially finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true
 end
