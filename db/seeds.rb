80dc7da13ac3afb04f11b55cd018953ca5555b0b
Arnaud Fiora <arnaud.fiora@gmail.com>
2019-11-12 17:13:35 +0100

    added admin seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,68 +1,102 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-User.all.destroy_all
-puts "About to add some users... "
 
-50.times do
-  User.create!(
-    last_name: Faker::Name.unique.last_name,
-    first_name: Faker::Name.unique.first_name,
-    password: "1234567",
-    email: Faker::Internet.unique.email,
-    avatar: Faker::Avatar.image
-  )
-end
+# User.destroy_all
+# puts "Destroyed all users"
 
+#----- CREATORS -----#
+# Admin creators
+puts "About to add some ADMIN creators... "
 
-puts "About to add some companies... "
-
-# Company harold
 attributes = [{
   last_name: 'Thiran',
   first_name: 'Harold',
-  email: 'harold_1998@live.be',
+  email: 'haroldcreator@sponsop.be',
   avatar: 'https://avatars.githubusercontent.com/harold9898',
-  company: 'Unilever',
   password: 'sponsop'
-},
-# Company Arnaud
-{
-    last_name: 'Fiora',
-    first_name: 'Arnaud',
-    email: 'arnaudfiora@hotmail.com',
-    avatar: 'https://avatars.githubusercontent.com/arnaudfiora',
-    password: 'sponsop',
-    company: 'Veja'
   },
-# Company Eloïse
   {
-    last_name: 'de Streel',
-    first_name: 'Eloïse',
-    email: 'Eloise@25927.be',
-    avatar: 'https://avatars.githubusercontent.com/elo29597',
-    password: 'sponsop',
-    company: 'Disney'
+  last_name: 'Fiora',
+  first_name: 'Arnaud',
+  email: 'arnaudcreator@sponsop.be',
+  avatar: 'https://avatars.githubusercontent.com/arnaudfiora',
+  password: 'sponsop',
   },
-  # Company Dylan
   {
-    last_name: 'Di Filippo',
-    first_name: 'Dylan',
-    email: 'Dylandifili@po.be',
-    avatar: 'https://avatars.githubusercontent.com/dylandifilippo',
-    password: 'sponsop',
-    company: 'Levis'
+  last_name: 'de Streel',
+  first_name: 'Eloïse',
+  email: 'eloisecreator@sponsop.be',
+  avatar: 'https://avatars.githubusercontent.com/elo29597',
+  password: 'sponsop',
+  },
+  {
+  last_name: 'Di Filippo',
+  first_name: 'Dylan',
+  email: 'dylancreator@sponsop.be',
+  avatar: 'https://avatars.githubusercontent.com/dylandifilippo',
+  password: 'sponsop',
   }]
 
-user = User.new {attributes}
-user.is_brand = true
-user.save
+User.create!(attributes)
 
+puts "4 ADMIN creators added"
 
+# puts "About to add some creators... "
 
+# 50.times do
+#   User.create!(
+#     last_name: Faker::Name.unique.last_name,
+#     first_name: Faker::Name.unique.first_name,
+#     password: "1234567",
+#     email: Faker::Internet.unique.email,
+#     avatar: Faker::Avatar.image
+#   )
+# end
 
+# puts "5O creators added"
+#--------------------#
+
+#----- BRANDS -----#
+# Admin brands
+puts "About to add some ADMIN brands... "
+
+attributes = [{
+  last_name: 'Thiran',
+  first_name: 'Harold',
+  email: 'haroldbrand@sponsop.be',
+  avatar: 'https://avatars.githubusercontent.com/harold9898',
+  is_brand: true,
+  company: 'Unilever',
+  password: 'sponsop'
+  },
+  {
+  last_name: 'Fiora',
+  first_name: 'Arnaud',
+  email: 'arnaudbrand@sponsop.be',
+  avatar: 'https://avatars.githubusercontent.com/arnaudfiora',
+  is_brand: true,
+  password: 'sponsop',
+  company: 'Veja'
+  },
+  {
+  last_name: 'de Streel',
+  first_name: 'Eloïse',
+  email: 'eloisebrand@sponsop.be',
+  avatar: 'https://avatars.githubusercontent.com/elo29597',
+  is_brand: true,
+  password: 'sponsop',
+  company: 'Disney'
+  },
+  {
+  last_name: 'Di Filippo',
+  first_name: 'Dylan',
+  email: 'dylanbrand@sponsop.be',
+  avatar: 'https://avatars.githubusercontent.com/dylandifilippo',
+  is_brand: true,
+  password: 'sponsop',
+  company: 'Levis'
+  }]
+
+User.create!(attributes)
+
+puts "4 ADMIN brands added"
+#--------------------#
