eaab87b5d6ac9effa400982328f1341240faec93
Harold Thiran <harold_1998@live.be>
2019-11-12 16:53:01 +0100

    adding actions to campaign controller and view

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,9 +1,10 @@
 class Campaign < ApplicationRecord
   belongs_to :user
-  has_one :age
+  belongs_to :age
   has_many :interests, through: :campaign_interests
 
   GENDER = ['Male', 'Female', 'Both']
 
   validates :gender, inclusion: { in: GENDER }
+  accepts_nested_attributes_for :age
 end
