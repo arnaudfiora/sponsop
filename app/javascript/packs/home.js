b2570d47292f9084e659be9ed70b7ffa5e5426c2
Arnaud Fiora <arnaud.fiora@gmail.com>
2019-11-13 23:58:30 +0100

    changed style of testimonials + added some JS

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,42 +1,44 @@
-// Make the page scroll to the log-in/sign-up section when clicking on the button
-const startButton = document.getElementById('get-started');
-const homeBanner = document.getElementById('home-banner');
-const homeBannerHeight = homeBanner.scrollHeight;
+import Typed from "typed.js"
 
-const scrollToFormSection = () => {
+// Make the page scroll to the log-in/sign-up section when clicking on the button
+const navbar = document.querySelector('.navbar-sponsop')
+const homeBanner = document.getElementById('home-banner');
+const learnMoreButton = document.getElementById('learn-more-button');
+
+const homeBannerHeight = homeBanner.scrollHeight;
+const navbarHeight = navbar.scrollHeight;
+const bannerSize = homeBannerHeight - navbarHeight
+
+const scrollToFirstSection = () => {
   window.scroll({
-    top: homeBannerHeight,
+    top: bannerSize,
     behavior: 'smooth'
   });
 };
 
-startButton.addEventListener('click', scrollToFormSection);
+learnMoreButton.addEventListener('click', scrollToFirstSection);
+// -------------------- //
 
-// Make the forms appear
-const brandButton = document.getElementById('button-brand');
-const creatorButton = document.getElementById('button-creator');
+// Make the navbar transparent on loading and then appearing after banner
+window.onload = (event) => {
+  navbar.classList.add('transparent');
+};
 
-const overlayLeft = document.querySelector('.overlay-left')
-const overlayRight = document.querySelector('.overlay-right');
-
-// Make the opposite panel move when clicking on a button
-brandButton.addEventListener('click', () => {
-    overlayRight.classList.toggle('right-panel-active');
-    scrollToFormSection();
+window.addEventListener('scroll', (event) => {
+  if (window.scrollY < 730) {
+    navbar.classList.add('transparent');
+  } else {
+    navbar.classList.remove('transparent');
+  }
 });
+// -------------------- //
 
-creatorButton.addEventListener('click', () => {
-    overlayLeft.classList.toggle('left-panel-active');
-    scrollToFormSection();
+// Adding the scrolling text in the banner
+var typed = new Typed('.type', {
+  strings: ["Sponsop"],
+  typeSpeed: 35,
+  backSpeed: 35,
+  backDelay: 1000,
+  loop: true,
 });
-
-// Putting it back in place when clicking on the remaining part of the div
-overlayRight.addEventListener('click', () => {
-    overlayRight.classList.remove('right-panel-active');
-    scrollToFormSection();
-});
-
-overlayLeft.addEventListener('click', () => {
-    overlayLeft.classList.remove('left-panel-active');
-    scrollToFormSection();
-});
+// -------------------- //
