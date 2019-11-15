5bcee90d26dd090cc812a69a4f6d899743735fcb
Arnaud Fiora <arnaud.fiora@gmail.com>
2019-11-15 16:56:00 +0100

    results page almost done - Need to work on dropdown list and finishing design details

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'campaign_interests/create'
+  get 'campaign_interests/destroy'
   devise_for :users
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
