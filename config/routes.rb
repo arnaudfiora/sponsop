38e79f4ca282217247c311020e82fc2deef7e1aa
Dylan Di Filppo <dylan.di.filippo@gmail.com>
2019-11-15 16:49:37 +0100

    add a chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'messages/create'
+  # get 'chat_rooms/show'
   devise_for :users
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
   resources :campaigns
   resources :channels
+  # resources :chat_room, only:[:show, :create]
+  get ('chat_rooms/:id'), to: 'chat_rooms#show', as: :chat_room
+  post ('chat_rooms/:chat_room_id/messages'), to: 'messages#create', as: :chat_room_messages
 end
