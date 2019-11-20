0724aeb113fc3bc08c237598ec4eaa1f6397bcbf
Harold Thiran <harold_1998@live.be>
2019-11-20 15:54:09 +0100

    link message from brand to influencer

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -5,15 +5,15 @@ class ChatRoomsController < ApplicationController
 
   def new
     @channel = Channel.find(params[:format])
-    user_1 = current_user.first_name
+    user_1 = current_user
     user_2 = @channel.user
 
 
     if existing_chatroom = ChatRoom.find_by( email:user_2.email)
       redirect_to chat_room_path(existing_chatroom)
     else
-      @chatroom = ChatRoom.new(name: " #{user_2.first_name}", user1: user_1, user2: user_2,email: user_2.email)
-      @chatroom.user = current_user
+      @chatroom = ChatRoom.new(name: " #{user_2.first_name}", user1: user_1, user2: user_2, email: user_2.email)
+      @chatroom.user = user_1
         if @chatroom.save
         redirect_to chat_room_path(@chatroom)
         end
